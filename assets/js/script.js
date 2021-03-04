4a8844dc3ffdd2af33bef2b3a2c035f5c20d2cf5
YesFactor <info.yesfactor@gmail.com>
2021-03-04 15:02:15 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+function FindOutNow() { 
+            window.open("https://www.youtube.com/channel/UC37QRCJznXwZ5xANpzDAWtQ", "_blank"); 
+        }
+
+function onButtonClick() {
+        Instamojo.open('https://www.instamojo.com/@donate_yesfactor');
+      }
+
+$(function(){
+	$(".typed").typed({
+		strings: ["WE WILL HELP YOU TO FIND YOUR","WHAT IS YOUR", "HAVE YOU GOT YOUR"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 100,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 40,
+		// time before backspacing
+		backDelay: 2000,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
