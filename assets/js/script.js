ec0bab61e9341f4f2e0d47cc3593db6b8cac2cac
YesFactor <info.yesfactor@gmail.com>
2021-03-05 23:10:17 +0530

    nav bar fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,41 @@ $(function(){
 		resetCallback: function() {}
 	});
 });
+
+
+$(document).ready(function(){
+	var contentSection = $('.content-section');
+	var navigation = $('nav');
+	navigation.on('click', 'a', function(event){
+		event.preventDefault(); //prevents previous event
+		smoothScroll($(this.hash));
+	});
+	
+	//update navigation on scroll...
+	$(window).on('scroll', function(){
+		updateNavigation();
+	})
+	//...and when the page starts
+	updateNavigation();
+	
+	/////FUNCTIONS
+	function updateNavigation(){
+		contentSection.each(function(){
+			var sectionName = $(this).attr('id');
+			var navigationMatch = $('nav a[href="#' + sectionName + '"]');
+			if( ($(this).offset().top - $(window).height()/2 < $(window).scrollTop()) &&
+				  ($(this).offset().top + $(this).height() - $(window).height()/2 > $(window).scrollTop()))
+				{
+					navigationMatch.addClass('active-section');
+				}
+			else {
+				navigationMatch.removeClass('active-section');
+			}
+		});
+	}
+	function smoothScroll(target){
+		$('body,html').animate({
+			scrollTop: target.offset().top
+		}, 800);
+	}
+});
